cd485a2ed3b3da84c74221aeb6d1328c726b985a
Gustaf Ekeberg <gustaf@gustafekeberg.se>
2016-04-05 21:55:45 +0200

    CSS file split in two parts. Also removed unused code for form setup fields.

--- src/main/resources/site/lib/contact-form-setup-fields/index.js
+++ src/main/resources/site/lib/contact-form-setup-fields/index.js
@@ -1,26 +1,24 @@
 var portalLib   = require('/lib/xp/portal');
-var contentLib  = require('/lib/xp/content');
 var thymeleaf   = require('/lib/xp/thymeleaf');
-var mailLib     = require('/lib/xp/mail');
-var utilDataLib = require('/lib/enonic/util/data');
-var i18nLib     = require('/lib/xp/i18n');
+// var contentLib  = require('/lib/xp/content');
+// var mailLib     = require('/lib/xp/mail');
+// var utilDataLib = require('/lib/enonic/util/data');
+// var i18nLib     = require('/lib/xp/i18n');
 
-// exports.get = function(request) { return handleGet(request); };
-// exports.post = function(request) {};
 exports.field = function(type){
 	var component = portalLib.getComponent();
-	var form = portalLib.getContent();
-	var formData = form.data;
-	var id = formData.id;
-	if (!id) formData.id = form._id;
+	var config = component.config;
+	// var form = portalLib.getContent();
+	// var formData = form.data;
+	// var id = formData.id;
+	// if (!id) formData.id = form._id;
 	var model = {
-		config: component.config,
-		formData: formData,
-		formInputType: type,
+		config: config,
+		input_type: type,
 	};
 	var view = resolve("contact-form-setup-fields.html");
 	var body = thymeleaf.render(view, model);
-	var style = '<link rel="stylesheet" href="' + assetUrl({path: '/css/style.css'}) + '">';
+	var style = '<link rel="stylesheet" href="' + assetUrl({path: '/css/form-builder-style.css'}) + '">';
 	return {
 		body: body,
 		contentType: 'text/html',
@@ -36,14 +34,14 @@ exports.field = function(type){
 // }
 
 
-function localize(key) {
-	return i18nLib.localize({key: key});
-}
+// function localize(key) {
+// 	return i18nLib.localize({key: key});
+// }
 
-function log( string ) {
-	var util = require('/lib/enonic/util/util');
-	util.log(string);
-}
+// function log( string ) {
+// 	var util = require('/lib/enonic/util/util');
+// 	util.log(string);
+// }
 
 function assetUrl(url){
 	return portalLib.assetUrl(url);
